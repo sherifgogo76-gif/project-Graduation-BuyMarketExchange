dba035874b39ad4a88722f1f3eac4facce41fcef
sherifgogo76-gif <sherifgogo76@gmail.com>
2026-05-11 16:57:32 +0300

    Update auth.validation.ts

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -2,7 +2,7 @@ import z, { email } from "zod";
 
 
 export const SignupVildation = z.strictObject({
-    name: z.string().min(2).max(52),
+    username: z.string().min(2).max(52),
     email: z.email(),
     password: z.string().regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/),
     confirmpassword: z.string().regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
