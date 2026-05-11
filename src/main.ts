4d50b24981d079d7fe8d7e6c1eebca55ac899910
sherifgogo76-gif <sherifgogo76@gmail.com>
2026-05-11 17:25:13 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 
+// async function bootstrap() {
+//   const port = process.env.PORT ?? 5000;
+//   const app = await NestFactory.create(AppModule);
+
+//   await app.listen(port, () => {
+//     console.log(`Server is running on port ${port} ✅🚀`);
+//   });
+// }
+
+// bootstrap();
+
+
 async function bootstrap() {
-  const port = process.env.PORT ?? 5000;
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(port, () => {
-    console.log(`Server is running on port ${port} ✅🚀`);
-  });
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, "0.0.0.0");
+
+  console.log(`Server is running on port ${port} ✅🚀`);
 }
 
 bootstrap();
