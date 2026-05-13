27cdf2682605b04051c63261bccf429200808bb5
sherifgogo76-gif <sherifgogo76@gmail.com>
2026-05-13 22:59:54 +0300

    Update send.email.ts

--- src/common/utiles/email/send.email.ts
+++ src/common/utiles/email/send.email.ts
@@ -1,13 +1,43 @@
+// import nodemailer, { type Transporter } from "nodemailer";
+// import type Mail from "nodemailer/lib/mailer";
+
+// export const sendemail = async (data: Mail.Options): Promise<void> => {
+//   const transporter: Transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.APP_EMAIL,
+//       pass: process.env.APP_PASSWORD,
+//     },
+//   });
+
+//   const info = await transporter.sendMail({
+//     from: `"ROUTE ACADEMY" <${process.env.APP_EMAIL}>`,
+//     ...data,
+//   });
+
+//   console.log("Message sent: %s", info.messageId);
+// };
+
+
 import nodemailer, { type Transporter } from "nodemailer";
 import type Mail from "nodemailer/lib/mailer";
 
 export const sendemail = async (data: Mail.Options): Promise<void> => {
+
   const transporter: Transporter = nodemailer.createTransport({
-    service: "gmail",
+
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+
     auth: {
       user: process.env.APP_EMAIL,
       pass: process.env.APP_PASSWORD,
     },
+
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
   });
 
   const info = await transporter.sendMail({
@@ -15,5 +45,5 @@ export const sendemail = async (data: Mail.Options): Promise<void> => {
     ...data,
   });
 
-  console.log("Message sent: %s", info.messageId);
+  console.log("✅ Message sent:", info.messageId);
 };
