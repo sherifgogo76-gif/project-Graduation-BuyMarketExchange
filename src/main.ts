6f1e471ec6b864bb9572c15ab06f2a073792eb10
Sherif Ahmed <sherifgogo76@gmail.com>
2026-05-16 18:57:26 +0300

    init project

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = process.env.PORT ?? 5000;
   const app = await NestFactory.create(AppModule);
+  
+  console.log("DB_URI =", process.env.DB_URI);
 
-  await app.listen(port, () => {
+  await app.listen(port,'0.0.0.0', () => {
     console.log(`Server is running on port ${port} ✅🚀`);
   });
 }
