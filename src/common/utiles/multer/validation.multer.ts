ddb9dc9fa7610bcbfc3b6ab838cf997ab8c69e95
sherifgogo76-gif <sherifgogo76@gmail.com>
2026-05-16 21:22:53 +0300

    Update validation.multer.ts

--- src/common/utiles/multer/validation.multer.ts
+++ src/common/utiles/multer/validation.multer.ts
@@ -1,3 +1,4 @@
 export const FileValidation = {
-    image: ["image/jpeg", "image/png", "image/gif"]
+    // image: ["image/jpeg", "image/png", "image/gif"]
+    image: ["image/jpeg", "image/jpg", "image/png", "image/gif", "image/webp", "image/heic", "image/heif"]
 }
