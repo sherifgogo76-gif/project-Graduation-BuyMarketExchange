2d1c62765e655958951c4517b17ad1fb12e93ac0
sherifgogo76-gif <sherifgogo76@gmail.com>
2026-05-16 21:07:41 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -17,9 +17,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(port, () => {
-    console.log(`Server is running on port ${port} ✅🚀`);
-  });
+  console.log("DB_URI =", process.env.DB_URI);
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, "0.0.0.0");
+
+  console.log(`Server is running on port ${port} ✅🚀`);
 }
 
 bootstrap();
